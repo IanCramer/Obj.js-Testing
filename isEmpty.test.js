0b50bc0c6143981961eb209c4a813f676a48bb42
Kenneth Cramer <i.cramer@live.com>
2019-02-12 12:12:24 -0800

    shallow compare tests

--- isEmpty.test.js
+++ isEmpty.test.js
@@ -1,69 +1,72 @@
 const obj = require('./obj');
 isEmpty = obj.isEmpty;
 // True on primitives, false on defined objects
+// True on iterables?
 
-test('isEmpty, no arguements', () =>
+describe('isEmpty', () =>
 {
-	expect(isEmpty()).toBe(true);
-});
+	test('no arguements', () =>
+	{
+		expect(isEmpty()).toBe(true);
+	});
 
-test('isEmpty, one undefined/null arguement', () =>
-{
-	expect(isEmpty(undefined)).toBe(true);
-	expect(isEmpty(null)).toBe(true);	
-});
+	test('one undefined/null arguement', () =>
+	{
+		expect(isEmpty(undefined)).toBe(true);
+		expect(isEmpty(null)).toBe(true);	
+	});
 
-test('isEmpty, multiple undefined/null arguement', () =>
-{
-	expect(isEmpty(undefined, null)).toBe(true);
-	expect(isEmpty(null, undefined, null)).toBe(true);	
-});
+	test('multiple undefined/null arguement', () =>
+	{
+		expect(isEmpty(undefined, null)).toBe(true);
+		expect(isEmpty(null, undefined, null)).toBe(true);	
+	});
 
-test('isEmpty, one boolean arguement', () =>
-{
-	expect(isEmpty(true)).toBe(true);
-	expect(isEmpty(false)).toBe(true);	
-});
+	test('one boolean arguement', () =>
+	{
+		expect(isEmpty(true)).toBe(true);
+		expect(isEmpty(false)).toBe(true);	
+	});
 
-test('isEmpty, multiple boolean arguement', () =>
-{
-	expect(isEmpty(true, false)).toBe(true);
-	expect(isEmpty(false, true, true)).toBe(true);	
-});
+	test('multiple boolean arguement', () =>
+	{
+		expect(isEmpty(true, false)).toBe(true);
+		expect(isEmpty(false, true, true)).toBe(true);	
+	});
 
-test('isEmpty, one number arguement', () =>
-{
-	expect(isEmpty(5)).toBe(true);
-	expect(isEmpty(3.75)).toBe(true);
-	expect(isEmpty(0)).toBe(true);
-	expect(isEmpty(-4)).toBe(true);	
-});
+	test('one number arguement', () =>
+	{
+		expect(isEmpty(5)).toBe(true);
+		expect(isEmpty(3.75)).toBe(true);
+		expect(isEmpty(0)).toBe(true);
+		expect(isEmpty(-4)).toBe(true);	
+	});
 
-test('isEmpty, multiple number arguement', () =>
-{
-	expect(isEmpty(5, 3.75, 0, -4)).toBe(true);
-	expect(isEmpty(3.75, 0, -4, 5)).toBe(true);
-	expect(isEmpty(0, -4, 5, 3.75)).toBe(true);
-	expect(isEmpty(-4, 5, 3.75, 0)).toBe(true);	
-});
+	test('multiple number arguement', () =>
+	{
+		expect(isEmpty(5, 3.75, 0, -4)).toBe(true);
+		expect(isEmpty(3.75, 0, -4, 5)).toBe(true);
+		expect(isEmpty(0, -4, 5, 3.75)).toBe(true);
+		expect(isEmpty(-4, 5, 3.75, 0)).toBe(true);	
+	});
 
-test('isEmpty, one string arguement. True on empty string.', () =>
-{
-	expect(isEmpty('true')).toBe(false);
-	expect(isEmpty('')).toBe(true);	
-});
+	test('one string arguement. True on empty string.', () =>
+	{
+		expect(isEmpty('true')).toBe(false);
+		expect(isEmpty('')).toBe(true);	
+	});
 
-test('isEmpty, multiple string arguement. True on first string empty.', () =>
-{
-	expect(isEmpty('true', '', 'another string')).toBe(false);
-	expect(isEmpty('', 'more strings')).toBe(true);	
-});
+	test('multiple string arguement. True on first string empty.', () =>
+	{
+		expect(isEmpty('true', '', 'another string')).toBe(false);
+		expect(isEmpty('', 'more strings')).toBe(true);	
+	});
 
-test('isEmpty, one object arguement. True on empty object.', () =>
-{
-	var chair = {numLegs: 4, material: 'wood', comfy: true};
-	var emptyObject = {};
-	expect(isEmpty(chair)).toBe(false);
-	expect(isEmpty(emptyObject)).toBe(true);
+	test('one object arguement. True on empty object.', () =>
+	{
+		var chair = {numLegs: 4, material: 'wood', comfy: true};
+		var emptyObject = {};
+		expect(isEmpty(chair)).toBe(false);
+		expect(isEmpty(emptyObject)).toBe(true);
+	});
 });
-
--- shallowCompare.test.js
+++ shallowCompare.test.js
@@ -1,7 +1,143 @@
 const obj = require('./obj');
 shallowCompare = obj.shallowCompare;
 
-test('Sample', () =>
+// Only succeeds on identical iterables
+
+describe('shallowCompare', () =>
 {
-	expect(2+2).toBe(4);
+	test('no arguements', () =>
+	{
+		expect(shallowCompare()).toBe(true);
+	});
+
+	test('one arguement, null/undefined', () =>
+	{
+		expect(shallowCompare(null)).toBe(true);
+		expect(shallowCompare(undefined)).toBe(true);
+	});
+
+	test('two arguements, null/undefined', () =>
+	{
+		expect(shallowCompare(null, undefined)).toBe(true);
+		expect(shallowCompare(undefined, null)).toBe(true);
+		expect(shallowCompare(undefined, undefined)).toBe(true);
+		expect(shallowCompare(null, null)).toBe(true);
+	});
+
+	test('three arguements, null/undefined', () =>
+	{
+		expect(shallowCompare(null, null, null)).toBe(true);
+		expect(shallowCompare(null, null, undefined)).toBe(true);
+		expect(shallowCompare(null, undefined, null)).toBe(true);
+		expect(shallowCompare(null, undefined, undefined)).toBe(true);
+		expect(shallowCompare(undefined, null, null)).toBe(true);
+		expect(shallowCompare(undefined, null, undefined)).toBe(true);
+		expect(shallowCompare(undefined, undefined, null)).toBe(true);
+		expect(shallowCompare(undefined, undefined, undefined)).toBe(true);
+	});
+
+	test('four (too many) arguements, null/undefined', () =>
+	{
+		expect(shallowCompare(null, null, null, null)).toBe(true);
+		expect(shallowCompare(null, null, null, undefined)).toBe(true);
+		expect(shallowCompare(null, null, undefined, null)).toBe(true);
+		expect(shallowCompare(null, null, undefined, undefined)).toBe(true);
+		expect(shallowCompare(null, undefined, null, null)).toBe(true);
+		expect(shallowCompare(null, undefined, null, undefined)).toBe(true);
+		expect(shallowCompare(null, undefined, undefined, null)).toBe(true);
+		expect(shallowCompare(null, undefined, undefined, undefined)).toBe(true);
+		expect(shallowCompare(undefined, null, null, null)).toBe(true);
+		expect(shallowCompare(undefined, null, null, undefined)).toBe(true);
+		expect(shallowCompare(undefined, null, undefined, null)).toBe(true);
+		expect(shallowCompare(undefined, null, undefined, undefined)).toBe(true);
+		expect(shallowCompare(undefined, undefined, null, null)).toBe(true);
+		expect(shallowCompare(undefined, undefined, null, undefined)).toBe(true);
+		expect(shallowCompare(undefined, undefined, undefined, undefined)).toBe(true);
+	});
+
+	test('one arguement, primitives. Fails on string', () =>
+	{
+		expect(shallowCompare(true)).toBe(true);
+		expect(shallowCompare(4)).toBe(true);
+		expect(shallowCompare(2.7)).toBe(true);
+		expect(shallowCompare('')).toBe(true);
+		expect(shallowCompare('A string')).toBe(false);
+	});
+
+	test('two identical strings', () =>
+	{
+		expect(shallowCompare('A string', 'A string')).toBe(true);
+	});
+
+	test('two same length strings', () =>
+	{
+		expect(shallowCompare('the string', 'its string')).toBe(false);
+	});
+
+	test('two different length strings', () =>
+	{
+		expect(shallowCompare('the string', 'A string')).toBe(false);
+	});
+
+	test('one object, one undefined', () =>
+	{
+		var chair = {legs: 4, material: 'wood', comfy: true};
+		expect(shallowCompare(chair, undefined)).toBe(false);
+	});
+
+	test('two objects, same keys same values', () =>
+	{
+		var obj1 = {p1: 1, p2: 'two', p3: true};
+		var obj2 = {p1: 1, p2: 'two', p3: true};
+		expect(shallowCompare(obj1, obj2)).toBe(true);
+	});
+
+	test('two objects, same keys different values', () =>
+	{
+		var obj1 = {p1: 1, p2: 'two', p3: true};
+		var obj2 = {p1: false, p2: 2, p3: 'three'};
+		expect(shallowCompare(obj1, obj2)).toBe(false);
+	});
+
+	test('two objects, different keys same values', () =>
+	{
+		var obj1 = {p11: 1, p12: 'two', p13: true};
+		var obj2 = {p21: 1, p22: 'two', p23: true};
+		expect(shallowCompare(obj1, obj2)).toBe(false);
+	});
+
+	test('two objects of different size', () =>
+	{
+		var obj1 = {p1: 1, p2: 'two', p3: true};
+		var obj2 = {p1: 'one', p2: 2, p3: false, p4: 2.7, p5: 'another string'};
+		expect(shallowCompare(obj1, obj2)).toBe(false);
+	});
+
+	test('two different objects with specified keys the same key and value', () =>
+	{
+		var obj1 = {a: 1, b: false, c: 'string', d: 3.4, e: true, f: 'more string'};
+		var obj2 = {a1: 12, b1: 6, c: 'string', d: 3.4, e: true, f1: 16};
+		expect(shallowCompare(obj1, obj2, ['c', 'd'])).toBe(true);
+	});
+
+	test('two different objects with specified keys the same key different value', () =>
+	{
+		var obj1 = {a: 1, b: false, c: 'string', d: 3.4, e: true, f: 'more string'};
+		var obj2 = {a1: 12, b1: 6, c: 'string', d: 7.4, e: true, f1: 16};
+		expect(shallowCompare(obj1, obj2, ['c', 'd'])).toBe(false);
+	});
+
+	test('two different objects with specified key not in one object', () =>
+	{
+		var obj1 = {a: 1, b: false, c: 'string', d: 3.4, e: true, f: 'more string'};
+		var obj2 = {a1: 12, b1: 6, c1: 'string', d: 3.4, e: true, f1: 16};
+		expect(shallowCompare(obj1, obj2, ['c', 'd'])).toBe(false);
+	});
+
+	test('two different objects with specified key not in either object', () =>
+	{
+		var obj1 = {a: 1, b: false, c: 'string', d: 3.4, e: true, f: 'more string'};
+		var obj2 = {a1: 12, b1: 6, c1: 'string', d: 7.4, e: true, f1: 16};
+		expect(shallowCompare(obj1, obj2, ['x', 'y'])).toBe(true);
+	});
 });

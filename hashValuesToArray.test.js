cd7da3dfaee668b8ad2aa766728acc49dee349f3
Kenneth Cramer <i.cramer@live.com>
2019-02-13 12:12:06 -0800

    testing for hashValuesToArray

--- hashValuesToArray.test.js
+++ hashValuesToArray.test.js
@@ -1,7 +1,37 @@
 const obj = require('./obj');
 hashValuesToArray = obj.hashValuesToArray;
 
-test('Sample', () =>
+
+
+describe('hashValuesToArray', () =>
 {
-	expect(2+2).toBe(4);
+	test('no arguements', () =>
+	{
+		expect(hashValuesToArray()).toEqual( [] );
+	});
+
+	test('one primitive arguement', () =>
+	{
+		expect(hashValuesToArray(true)).toEqual( [] );
+		expect(hashValuesToArray(4)).toEqual( [] );
+		expect(hashValuesToArray('')).toEqual( [] );
+		expect(hashValuesToArray('str')).toEqual( ['s', 't', 'r'] );
+	});
+
+	test('one object arguement', () =>
+	{
+		var obj = {p1: true, p2: 2, p3: 'three'};
+		expect(hashValuesToArray(obj)).toEqual( [true, 2, 'three'] );
+		expect(hashValuesToArray({})).toEqual( [] );
+	});
+
+	test('one function arguement', () =>
+	{
+		var func = function(x) {return x;}
+		expect(hashValuesToArray(func)).toEqual( [] );
+	});
+
+	// Testing with a second paramenter wil only sort/not sort the array
+	// Second parameter is evaluated on basis of truthiness
+	// Third + parameters will be ignored.
 });
